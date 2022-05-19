49a2df791e71380b2c2c16e025b250f6c45d13b7
Oscar Enrique Marchena <omarchena@adm.unapec.edu.do>
2022-05-19 13:53:29 -0400

    removido keya

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,14 +3,7 @@ import 'firebase/firestore'
 import 'firebase/auth'
 import 'firebase/storage'
 
-const firebaseConfig = {
-  apiKey: "AIzaSyA73uKCpcbRnGNC__lnxl6zLWlELD0qBio",
-  authDomain: "muso-gamers.firebaseapp.com",
-  projectId: "muso-gamers",
-  storageBucket: "muso-gamers.appspot.com",
-  messagingSenderId: "1056089785427",
-  appId: "1:1056089785427:web:06e304bca8994362a9b2a6"
-};
+
 
   firebase.initializeApp(firebaseConfig)
 
